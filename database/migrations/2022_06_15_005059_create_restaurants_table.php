a8e4261259962ea047ef1c7e0a62df4dea5e36be
priyavanjarapu <priyavanjarapu98@gmail.com>
2022-06-17 18:46:17 +0530

    Restaurant table schema added

--- database/migrations/2022_06_15_005059_create_restaurants_table.php
+++ database/migrations/2022_06_15_005059_create_restaurants_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
+            $table->string('image');
             $table->string('location');
             $table->float('rating');
             $table->json('food_items');
