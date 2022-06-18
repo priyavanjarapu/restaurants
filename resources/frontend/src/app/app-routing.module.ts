787b6ece41c283758e4114248dfbba8e18fca5f9
priyavanjarapu <priyavanjarapu98@gmail.com>
2022-06-18 21:56:13 +0530

    Bootstrap list static HTML code added

--- resources/frontend/src/app/app-routing.module.ts
+++ resources/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RestuarantCreateComponent} from "./restuarant-create/restuarant-create.component";
+import {RestuarantListComponent} from "./restuarant-list/restuarant-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/restaurants/list', pathMatch: 'full'},
+  {path: 'restaurants/add', component: RestuarantCreateComponent},
+  {path: 'restaurants/list', component: RestuarantListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
