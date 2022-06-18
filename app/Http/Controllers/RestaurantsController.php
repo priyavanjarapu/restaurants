b320ea5f0b9a3edde53b37f2a165c02a2c72891e
priyavanjarapu <priyavanjarapu98@gmail.com>
2022-06-18 06:36:35 +0530

    API routes created for CRUD, controller functions

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurants;
 use Illuminate\Http\Request;
 
 class RestaurantsController extends Controller
 {
-    //
+    // Get the list of ALL restuarants from the database
+    public function index(Request $request)
+    {
+        $restuarants = Restaurants::all();
+
+        return response($restuarants, 200);
+    }
+
+    // Fetch specific restuarant details
+    public function fetchResturant($id)
+    {
+    }
+
+    // Store restuarant details into database
+    public function store(Request $request)
+    {
+    }
+
+    // Update specific restuarant details in the database
+    public function update(Request $request, $id)
+    {
+    }
+
+    // Delete specific restuarant from the database
+    public function delete($id)
+    {
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RestaurantsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Defining API route for restuarant
+Route::group(['prefix' => 'restuarants'], function () {
+    Route::get('', [RestaurantsController::class, 'index']);
+    Route::get('/{id}', [RestaurantsController::class, 'fetchResturant']);
+    Route::post('', [RestaurantsController::class, 'store']);
+    Route::post('/update/{id}', [RestaurantsController::class, 'update']);
+    Route::post('/delete/{id}', [RestaurantsController::class, 'delete']);
 });
