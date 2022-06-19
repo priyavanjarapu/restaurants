7b40541161ac5e14331a10edfc60061e187862c1
priyavanjarapu <priyavanjarapu98@gmail.com>
2022-06-19 08:25:33 +0530

    Restaurant create form functionality

--- resources/frontend/src/app/app.module.ts
+++ resources/frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RestuarantListComponent } from './restuarant-list/restuarant-list.component';
-import { RestuarantCreateComponent } from './restuarant-create/restuarant-create.component';
-import { RestuarantDetailsComponent } from './restuarant-details/restuarant-details.component';
-import { RestuarantDeleteComponent } from './restuarant-delete/restuarant-delete.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RestuarantListComponent} from './restuarant-list/restuarant-list.component';
+import {RestuarantCreateComponent} from './restuarant-create/restuarant-create.component';
+import {RestuarantDetailsComponent} from './restuarant-details/restuarant-details.component';
+import {RestuarantDeleteComponent} from './restuarant-delete/restuarant-delete.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -18,9 +19,11 @@ import { RestuarantDeleteComponent } from './restuarant-delete/restuarant-delete
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- resources/frontend/src/app/restuarant-create/restuarant-create.component.ts
+++ resources/frontend/src/app/restuarant-create/restuarant-create.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-restuarant-create',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestuarantCreateComponent implements OnInit {
 
-  constructor() { }
+  public form = new FormGroup({
+    image: new FormControl(null, Validators.required),
+    title: new FormControl(null, Validators.required),
+    description: new FormControl(null, Validators.required),
+    location: new FormControl(null, Validators.required),
+    rating: new FormControl(null, Validators.required),
+    food_items: new FormControl([], Validators.required)
+  });
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  submit() {}
 }
